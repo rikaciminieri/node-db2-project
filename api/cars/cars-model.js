726d9ba8bfbcc3bce97c369d986019a0a5ff7921
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-19 17:35:26 -0700

    WIP

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,13 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  return db("cars");
+};
 
 const getById = () => {
   // DO YOUR MAGIC
-}
+};
 
 const create = () => {
   // DO YOUR MAGIC
-}
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,6 @@
 // DO YOUR MAGIC
+const router = require("express").Router()
+
+const Cars = require("./cars-model")
+
+module.exports = router;
